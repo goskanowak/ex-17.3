8274f63ee60684695ab256370aefd4c413b2b182
Małgorzata Nowak <goskanowak87@gmail.com>
2018-06-27 11:29:42 +0200

    Add js and json

---
+++ server.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const fs = require('fs');
+let stringifyFile = '';
+
+app.use(bodyParser.json());
+
+app.get('/getNote', function(req, res) {
+  fs.readFile('./test.json', 'utf8', function(err, data) {
+    if (err) throw err;
+    stringifyFile = data;
+    res.send(data);
+  });
+});
+
+app.post('/updateNote/:note', function(req, res) {
+  stringifyFile = req.params.note;
+  fs.writeFile('./test.json', stringifyFile, function(err) {
+    if (err) throw err;
+    console.log('file update');
+  });
+});
+
+app.use(function(req, res, next) {
+  res.status(404).send('Sorry, not found!');
+});
+
+app.listen(3000);
